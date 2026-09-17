c6a10f160b999fa0bad3eb7a09841d7799ad0329
hyunwoojung37 <hyunwoojung37@gmail.com>
2026-09-16 19:07:44 -0700

    Added exception handling to prevent any errors, fixed comments

--- main.py
+++ main.py
@@ -21,18 +21,24 @@ class TaskBlocker:
             return
         # checks if backup file exists
         self.backup_hosts()
-        # read hosts path or history and ip
-        with open(self.hosts_path, "r") as file:
-            content = file.read()
-
-        # adding websites to block into content
-        with open(self.hosts_path, "a") as file:
-            for site in self.sites:
-                block_entry = self.redirectedIP + " " + site
-                if block_entry not in content:
-                    file.write(f"{block_entry}\n")
-                else:
-                    pass
+        try:
+            # read hosts path or history and ip
+            with open(self.hosts_path, "r") as file:
+                content = file.read()
+            # adding websites to block into content
+            with open(self.hosts_path, "a") as file:
+                for site in self.sites:
+                    block_entry = self.redirectedIP + " " + site
+                    if block_entry not in content:
+                        file.write(f"{block_entry}\n")
+                    else:
+                        pass
+        except FileNotFoundError:
+            print("The file's host was not found")
+            return
+        except PermissionError:
+            print("You do not have access")
+            return
 
     def unblock_sites(self):
         # checking if admin
@@ -42,25 +48,32 @@ class TaskBlocker:
         # checks if backup file exists
         self.backup_hosts()
         # reads out each website stored
-        with open(self.hosts_path, "r") as file:
-            hosts_lines = file.readlines()
-        lines_to_keep = []
+        try:
+            with open(self.hosts_path, "r") as file:
+                hosts_lines = file.readlines()
+            lines_to_keep = []
 
-        # checks if websites in host lines matches hosts path
-        for hosts_line in hosts_lines:
-            should_remove = False
-            for site in self.sites:
-                block_entry = self.redirectedIP + " " + site
-                if block_entry in hosts_line:
-                    should_remove = True
-        # if so remove, else keep in new list
-            if should_remove is False:
-                lines_to_keep.append(hosts_line)
+            # checks if websites in host lines matches hosts path
+            for hosts_line in hosts_lines:
+                should_remove = False
+                for site in self.sites:
+                    block_entry = self.redirectedIP + " " + site
+                    if block_entry in hosts_line:
+                        should_remove = True
+            # if so remove, else keep in new list
+                if should_remove is False:
+                    lines_to_keep.append(hosts_line)
 
-        # overwrite all hosts path with just the lines we keep
-        with open(self.hosts_path, "w") as file:
-            for hosts_line in lines_to_keep:
-                file.write(hosts_line)
+            # overwrite all hosts path with just the lines we keep
+            with open(self.hosts_path, "w") as file:
+                for hosts_line in lines_to_keep:
+                    file.write(hosts_line)
+        except FileNotFoundError:
+            print("The file's host was not found")
+            return
+        except PermissionError:
+            print("You do not have access")
+            return
 
     def backup_hosts(self):
         if os.path.exists("hosts_backup.txt"):
